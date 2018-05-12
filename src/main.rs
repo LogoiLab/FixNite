f8a8f8889734a6fcab68024d2210720fa3208285
Chad Baxter <cbaxter@mail.umw.edu>
2018-05-11 20:41:10 -0500

    recovered lost local changes

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::*;
+use std::io::prelude::*;
+
+fn main() {
+    let mut replay_file = match File::open(std::env::args().next_back().unwrap()){
+        Ok(o) => o,
+        Err(e) => {
+            panic!("{}", e);
+        },
+    };
+    let mut replay_data = Vec::new();
+    match replay_file.read_to_end(&mut replay_data){
+        Ok(_) => {
+            let offset: usize = 16;
+            if replay_data[offset] != 43 as u8 {
+                replay_data[offset] = 43 as u8;
+                replay_data[offset + 1] = 43 as u8;
+                replay_data[offset + 2] = 62 as u8;
+            }
+
+            let mut output_file = match File::create("foo.test"){
+                Ok(o) => o,
+                Err(e) => {
+                    panic!("{}", e);
+                },
+            };
+            output_file.write_all(replay_data.as_slice());
+
+            output_file.sync_all();
+        },
+         _ => (),
+    }
+
+}
