259814d2888d062a82646d74aa263b292c464b8b
Chad Baxter <cbaxter@mail.umw.edu>
2018-05-16 19:31:09 -0500

    published library

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod parse;
+pub mod modify;
+pub mod render;
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
-use std::fs::*;
-use std::io::prelude::*;
+extern crate fixnite_lib as fixnite;
 
 fn main() {
     let path = std::env::args().next_back().unwrap();
-    let mut replay_file = match File::open(&path){
-        Ok(o) => o,
-        Err(e) => panic!("{}", e),
-    };
-    let mut replay_data = Vec::new();
-    match replay_file.read_to_end(&mut replay_data){
-        Ok(_) => {
-            let offset: usize = 16;
-            if replay_data[offset] != 42 as u8 {
-                replay_data[offset] = 42 as u8;
-                replay_data[offset + 1] = 42 as u8;
-                replay_data[offset + 2] = 61 as u8;
-            }
-
-            let mut output_file = match File::create(format!("fixed-{}", path.as_str())) {
-                Ok(o) => o,
-                Err(e) => panic!("{}", e),
-            };
-            match output_file.write_all(replay_data.as_slice()) {
-                Ok(_) => (),
-                Err(e) => panic!("{}", e),
-            }
-
-            match output_file.sync_all() {
-                Ok(_) => (),
-                Err(e) => panic!("{}", e),
-            }
-        },
-         Err(e) => panic!("{}", e),
-    }
-
+    fixnite::modify::modify::old_fix(path);
 }
