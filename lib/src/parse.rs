ec5a3fded3f27b696889e3a75aad1de074d31c56
Chad Baxter <cbaxter@mail.umw.edu>
2018-06-03 15:29:44 -0500

    started work on parser.

--- lib/src/parse.rs
+++ lib/src/parse.rs
@@ -1,7 +1,18 @@
 extern crate nom;
 
-pub mod parse {
-    pub fn read_file() {
-        unimplemented!();
-    }
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct replay_file {
+    name: String,
+    size: usize,
+    content: String,
+}
+
+pub fn load_file(filename: String) -> replay_file {
+    let mut f = File::open(&filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    return replay_file{name: filename, size: contents.len(), content: contents};
 }
