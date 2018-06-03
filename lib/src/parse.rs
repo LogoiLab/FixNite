9fa2618b499786a9face3a81f0e62997bdd611da
Chad Baxter <cbaxter@mail.umw.edu>
2018-06-03 15:40:00 -0500

    cleaning

--- lib/src/parse.rs
+++ lib/src/parse.rs
@@ -1,18 +1,21 @@
 extern crate nom;
 
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-pub struct replay_file {
+pub struct ReplayFile {
     name: String,
     size: usize,
     content: String,
 }
 
-pub fn load_file(filename: String) -> replay_file {
+pub fn load_file(filename: String) -> ReplayFile {
     let mut f = File::open(&filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
-    return replay_file{name: filename, size: contents.len(), content: contents};
+    return ReplayFile {
+        name: filename,
+        size: contents.len(),
+        content: contents
+    };
 }
